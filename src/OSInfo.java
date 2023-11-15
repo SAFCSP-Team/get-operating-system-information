94798d2edb9a6a3657e542428b29e56173e083e6
malekcoders <alomayri970@outlook.com>
2023-11-15 10:27:28 +0300

    init

---
+++ src/OSInfo.java
@@ -0,0 +1,8 @@
+public class OSInfo {
+    public static void getOSInfo() {
+        /* Your code here */
+    }
+    public static void main(String[] args) {
+        /* Call getOSInfo to output OS information */
+    }
+}
